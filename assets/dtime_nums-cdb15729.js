4692c1e8e18d95707df1a21d0bd2517c534f29c9
LegixCode <LegixCode@users.noreply.github.com>
2023-03-24 21:40:49 +0000

    deploy: edda2ad3ff882d275a8f9200bda3cd1c9ca27e35

---
+++ assets/dtime_nums-cdb15729.js
@@ -0,0 +1,7 @@
+const a=`function dtime_nums(offset) {
+    const date = new Date()
+    date.setDate(date.getDate() + offset)
+    let day = date.getDate().toString().padStart(2, 0);
+    let month = (date.getMonth() + 1).toString().padStart(2,0);
+    document.write({format});
+}`;function Y(e){var t="";switch(e){case"DD.MM.YYYY":t="day + '.' + month + '.' + date.getFullYear()";break;case"DD/MM/YYYY":t="day + '/' + month + '/' + date.getFullYear()";break;case"DD-MM-YYYY":t="day + '-' + month + '-' + date.getFullYear()";break;case"YYYY-MM-DD":t="date.getFullYear() + '-' + month + '-' + day";break;case"MM-DD-YYYY":t="month + '-' + day + '-' + date.getFullYear()";break;case"MM/DD/YYYY":t="month + '/' + day + '/' + date.getFullYear()";break}return a.replace("{format}",t)}export{Y as getDTimeNums};
